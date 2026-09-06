7022d786071e6d868dcd5cf19a8c30db7d811537
King Star <mcxin.y@gmail.com>
2026-09-07 04:56:37 +0800

    Fix custom header schema freshness (#358)
    Co-authored-by: Paul Carleton <paulcarletonjr@gmail.com>

--- src/scenarios/client/http-custom-headers.ts
+++ src/scenarios/client/http-custom-headers.ts
@@ -278,7 +278,7 @@ export class HttpCustomHeadersScenario extends BaseHttpScenario {
       id: request.id,
       result: {
         resultType: 'complete',
-        ttlMs: 0,
+        ttlMs: 300000,
         cacheScope: 'private',
         tools: [
           {
